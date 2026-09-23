f1a6f0d2be94784e3c4f0ecc36132b5e3091013c
Danielllanos <pcdoctorbermejo@gmail.com>
2026-09-23 18:59:17 -0400

    Ejercicio 8
    codigo completado

---
+++ practica_pregunta8.dart
@@ -0,0 +1,5 @@
+import 'package:practica_pregunta8/practica_pregunta8.dart' as practica_pregunta8;
+
+void main(List<String> arguments) {
+  print('Hello world: ${practica_pregunta8.calculate()}!');
+}
